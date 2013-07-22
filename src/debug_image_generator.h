004e6d0f033b44266d89911cbfec0c3e165f2d91
loganek <marcin.kolny@gmail.com>
2013-07-22 02:13:44 +0200

    added another debug operator - arrays of flow

--- src/debug_image_generator.h
+++ src/debug_image_generator.h
@@ -10,6 +10,10 @@
 
 #include "detector.h"
 #include <tuple>
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+
 struct FeatureListDebugOperator
 {
 	static void Process(cv::Mat& image, const Detector::DebugInfo& debugInfo)
@@ -19,6 +23,41 @@ struct FeatureListDebugOperator
 
 		for (auto center : debugInfo.features[1])
 			cv::circle(image, center, 3, cv::Scalar(0, 255, 255));
+	}
+};
+
+struct ArrowFlowDebugOperator
+{
+private:
+
+	static void DrawArrow(cv::Mat& image, cv::Point2f second, cv::Point2f first)
+	{
+		cv::Scalar color(0, 255, 0);
+		cv::Point2f p(first.x, first.y),
+				q(second.x, second.y);
+		double angle = atan2(p.y - q.y, p.x - q.x);
+
+		cv::line( image, p, q, color);
+
+		p.x = q.x + 9 * cos(angle + M_PI_4);
+		p.y = q.y + 9 * sin(angle + M_PI_4);
+		cv::line( image, p, q, color);
+
+		p.x = q.x + 9 * cos(angle - M_PI_4);
+		p.y = q.y + 9 * sin(angle - M_PI_4);
+		cv::line( image, p, q, color);
+	}
+
+public:
+	static void Process(cv::Mat& image, const Detector::DebugInfo& debugInfo)
+	{
+		for (size_t i = 0; i < debugInfo.featureStatus.size(); i++)
+		{
+			if (debugInfo.featureStatus[i])
+			{
+				DrawArrow(image, debugInfo.features[0][i], debugInfo.features[1][i]);
+			}
+		}
 	}
 };
 
